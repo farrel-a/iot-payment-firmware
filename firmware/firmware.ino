0dcd2132581b7efaeee16521b67a91803fef7c2c
farrel-a <13520110@std.stei.itb.ac.id>
2024-03-26 22:28:36 +0700

    feat: add topup credit

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -9,8 +9,8 @@
 // Macro
 #define SCREEN_WIDTH 128 
 #define SCREEN_HEIGHT 64
-#define LED_PIN 2
 #define BUTTON_PIN 0
+#define LED_PIN 2
 
 // WiFi
 const char *ssid = "-"; 
@@ -21,7 +21,8 @@ const char *mqtt_broker = "free.mqtt.iyoti.id";
 const int mqtt_port = 1883;
 const char *topic_payment_log = "/13520110/payment/log";
 const char *topic_balance_check = "/13520110/balance/check";
-const char *client_id = "ESP32Client13520110";
+const char *topic_balance_topup = "/13520110/balance/topup";
+const char *client_id = "ESP32Client13520110IoTPayment";
 
 // Global Variables
 int credit = 210000;
@@ -30,6 +31,19 @@ WiFiClient espClient;
 PubSubClient client(espClient);
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
 bool deduct = false;
+
+bool isNumeric(const char* str) {
+    if (*str == '\0') {
+        return false;
+    }
+    while (*str != '\0') {
+        if (!isdigit(*str)) {
+            return false;
+        }
+        str++;
+    }
+    return true;
+}
 
 void connect_wifi() {
   WiFi.begin(ssid, password);
@@ -49,19 +63,31 @@ void connect_broker() {
     client.connect(client_id);
     delay(10);
   }
-  Serial.println("MQTT Broker connected");
   client.subscribe(topic_balance_check);
+  client.subscribe(topic_balance_topup);
+  Serial.println("MQTT Broker connected");
 }
 
 void callback(char *topic_recv, byte *payload, unsigned int length) {
   String msg;
+  for (int i = 0; i < length; i++) {
+    msg += (char)payload[i];
+  }
+  String credit_str = String(credit);
   if (String(topic_recv) == String(topic_balance_check)) {
-    for (int i = 0; i < length; i++) {
-      msg += (char)payload[i];
-    }
     if (msg == "check") {
-      String credit_str = String(credit);
       client.publish(topic_balance_check, credit_str.c_str()); 
+      client.loop();
+    }
+  }
+  else if (String(topic_recv) == String(topic_balance_topup)) {
+    if (isNumeric(msg.c_str())) {
+      int topup = atoi(msg.c_str());
+      credit += topup;
+      credit_str = String(credit);
+      client.publish(topic_payment_log, msg.c_str());
+      client.publish(topic_balance_check, credit_str.c_str());
+      write_oled("Credit\nAdded"); delay(1000); write_oled("E-Wallet\nReady");
       client.loop();
     }
   }
@@ -81,26 +107,27 @@ void write_oled(String text) {
 }
 
 void setup() {
+  // Pin Settings
   pinMode(BUTTON_PIN, INPUT);
   pinMode(LED_PIN, OUTPUT);
+  attachInterrupt(BUTTON_PIN, isr, FALLING);
 
   // OLED Setup
-  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3D for 128x64
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
     Serial.println(F("SSD1306 allocation failed"));
     for(;;);
   }
+
   // Connect to Wi-Fi
   write_oled("Connecting\nWiFi");
   connect_wifi();
-  write_oled("WiFi\nConnected"); delay(10);
+  write_oled("WiFi\nConnected"); delay(1000);
 
   // Connect to MQTT Broker
   write_oled("Connecting\nBroker");
   connect_broker();
-  write_oled("Broker\nConnected"); delay(10);
+  write_oled("Broker\nConnected"); delay(1000);
   write_oled("E-Wallet\nReady");
-
-  attachInterrupt(BUTTON_PIN, isr, FALLING);
 }
 
 void loop() {
@@ -111,6 +138,7 @@ void loop() {
       write_oled("Credit\nDeducted");
       String log = "-" + String(20000);
       client.publish(topic_payment_log, log.c_str());
+      client.publish(topic_balance_check, String(credit).c_str());
       while(millis() - start_time < 5000){
         digitalWrite(LED_PIN, HIGH);
         client.loop();
